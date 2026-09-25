6ff9f5b22a7709227967cf35a329f88c33271319
seongju-p <krghuw@gmail.com>
2026-09-26 02:21:51 +0900

    feat: add ClassicFy dark theme

--- android/app/src/main/java/com/classicfy/app/ui/theme/Color.kt
+++ android/app/src/main/java/com/classicfy/app/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.classicfy.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ClassicFyBackground = Color(0xFF0D0D0D)
+val ClassicFySurface = Color(0xFF1A1A1A)
+val ClassicFySurfaceVariant = Color(0xFF252525)
+val ClassicFyAccent = Color(0xFFF4C542)
+val ClassicFyOnAccent = Color(0xFF0D0D0D)
+val ClassicFyTextPrimary = Color(0xFFF5F5F0)
+val ClassicFyTextSecondary = Color(0xFFB8B8B3)
